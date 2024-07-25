bcdf018007fc07155c7f6616cf4b555b014a223a
raffaell95 <raffaell95@live.com>
2024-07-25 00:28:22 -0300

    add .gitignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rafaelribeiro/Documents/codeflix-catalog-admin/venv"
+set -gx VIRTUAL_ENV "/Users/rafaelribeiro/Documents/codeflix-fullsycle/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
