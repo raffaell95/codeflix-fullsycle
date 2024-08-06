67c99326bd3e4d9db47ab2dff90aaaadee760392
raffaell95 <raffaell95@live.com>
2024-08-06 20:50:11 -0300

    add new files

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,27 +1,79 @@
+from uuid import UUID
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_201_CREATED , HTTP_204_NO_CONTENT
 
+from core.category.application.use_cases.create_category import CreateCategory, CreateCategoryRequest
+from core.category.application.use_cases.exceptions import CategoryNotFound
+from core.category.application.use_cases.get_category import GetCategory, GetCategoryRequest
+from core.category.application.use_cases.update_category import UpdateCategory, UpdateCategoryRequest
 from django_project.category_app.repository import DjangoORMCategoryRepository
+from django_project.category_app.serializers import CreateCategoryRequestSerializer, CreateCategoryResponseSerializer, ListCategoryResponseSerializer, RetrieveCategoryRequestSerializer, RetrieveCategoryResponseSerializer, UpdateCategoryRequestSerializer
 from src.core.category.application.use_cases.list_category import ListCategory, ListCategoryRequest
 
 # Create your views here.
 class CategoryViewSet(viewsets.ViewSet):
     def list(self, request: Request) -> Response:
 
-        input = ListCategoryRequest()
         use_case = ListCategory(repository=DjangoORMCategoryRepository())
-        output = use_case.execute(input)
+        response = use_case.execute(request=ListCategoryRequest())
 
-        categories = [
-            {
-                "id": str(category.id),
-                "name": category.name,
-                "description": category.description,
-                "is_active": category.is_active
-            } for category in output.data
-        ]
+        serializer = ListCategoryResponseSerializer(instance=response)
 
-        return Response(status=HTTP_200_OK, data=categories)
+        return Response(status=HTTP_200_OK, data=serializer.data)
+    
+    def retrieve(self, request: Request, pk=None):
+        serializer = RetrieveCategoryRequestSerializer(data={"id": pk})
+        serializer.is_valid(raise_exception=True)
+        
+        use_case = GetCategory(repository=DjangoORMCategoryRepository())
+        
+        try:
+            result = use_case.execute(request=GetCategoryRequest(id=serializer.validated_data["id"]))
+        except CategoryNotFound:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+        category_output = RetrieveCategoryResponseSerializer(instance=result)
+
+        return Response(
+            status=HTTP_200_OK,
+            data=category_output.data
+        )
+    
+    def create(self, request: Request) -> Response:
+        serializer = CreateCategoryRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        input = CreateCategoryRequest(**serializer.validated_data)
+        use_case = CreateCategory(repository=DjangoORMCategoryRepository())
+        output = use_case.execute(request=input)
+        
+        return Response(
+            status=HTTP_201_CREATED,
+            data=CreateCategoryResponseSerializer(instance=output).data
+        )
+    
+    def update(self, request: Request, pk=None) -> Response:
+        serializer = UpdateCategoryRequestSerializer(
+            data={
+                **request.data,
+                "id": pk
+            }
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        input = UpdateCategoryRequest(**serializer.validated_data)
+        use_case = UpdateCategory(repository=DjangoORMCategoryRepository())
+        
+        try:
+            use_case.execute(request=input)
+        except CategoryNotFound:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+        return Response(status=HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request: Request, pk=None) -> Response:
+        return Response(status=HTTP_204_NO_CONTENT)
